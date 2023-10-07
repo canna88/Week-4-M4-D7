9dc37d817818a5dd7970b980abde064469cfe2fc
canna88 <97223496+canna88@users.noreply.github.com>
2023-10-07 09:30:45 +0200

    first-commit
    Implement add-button function

--- script_back_office.js
+++ script_back_office.js
@@ -9,8 +9,18 @@ const requestOptionsGet = {
     // Aggiungi altri header se necessario
   },
 };
-
+const requestOptionsPost = {
+  method: "POST",
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTFmMTQxOWM3Mjg4NzAwMTg4N2ZmMWIiLCJpYXQiOjE2OTY1MzU1NzcsImV4cCI6MTY5Nzc0NTE3N30.7UuxGWrA8TVoFpfvg1a-mX0FXSBmdigPkRW-UNqC6h8",
+    // Aggiungi altri header se necessario
+  },
+};
 const productList = document.querySelector(".product-list-container");
+
+// Identificazione pulsanti nel DOM
+const addButton = document.querySelector(".add-button");
 
 // Funzione per caricare i libri iniziali dalla API e visualizzarli
 function loadProducts(bookList) {
@@ -34,8 +44,8 @@ function loadProducts(bookList) {
           <span>${formattedPrice}</span>
       </div>
       <div class="col-2">
-          <button class="btn btn-primary">Edit</button>
-          <button class="btn btn-danger">Delete</button>
+          <button class="btn btn-primary edit-button-row">Edit</button>
+          <button class="btn btn-danger delete-button-row">Delete</button>
       </div>
     </div>
 
@@ -58,6 +68,41 @@ function getProducts(link) {
     });
 }
 
+async function addProduct() {
+  event.preventDefault();
+  const formAdd = document.querySelector("#form-add");
+
+  const newProductData = JSON.stringify({
+    name: formAdd.querySelector("#name").value,
+    description: formAdd.querySelector("#description").value,
+    brand: formAdd.querySelector("#brand").value,
+    imageUrl: formAdd.querySelector("#imageUrl").value,
+    price: formAdd.querySelector("#price").value,
+  });
+
+  console.log(newProductData);
+
+  // Funzione per aggiungere il prodotto
+
+  try {
+    const response = await fetch(linkProducts, {
+      requestOptionsPost,
+      body: newProductData,
+    });
+    if (response.ok) {
+      const responseData = await response.json();
+      console.log("Prodotto aggiunto con successo:", responseData);
+      getProducts(linkProducts)
+    } else {
+      console.error("Errore nella richiesta:", response.status, response.statusText);
+
+    }
+  } catch (error) {
+    console.error("Errore nella richiesta:", error.message);
+  }
+}
+
 // FUNZIONI: ESECUZIONE
 
 getProducts(linkProducts); // Chiamiamo la funzione getProducts() per ottenere e visualizzare i libri iniziali dalla API
+addButton.addEventListener("click", addProduct);
